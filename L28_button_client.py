b592cedf0a5137824f15728647686b9952afb77d
Erik Jagre <erik.jagre@educ.goteborg.com>
2024-01-13 17:48:40 +0100

    Removed with/as, added graceful shutdown

--- L28_button_client.py
+++ L28_button_client.py
@@ -5,21 +5,25 @@ import socket
 import gpiozero
 import time
 
-HOST = "192.168.78.219" #Host to connect to
+HOST = "192.168.0.89" #Host to connect to
 PORT = 65432 		#Listen port
 
 def my_function():
 	print('Button was pressed!')
 
-	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-		s.connect((HOST, PORT))
-		s.sendall(b'Button was pressed!')
-		s.close()
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((HOST, PORT))
+	s.sendall(b'Button was pressed!')
+	s.close()
+
 
 button = gpiozero.Button(21)
 button.when_pressed = my_function
 
-
 while True:
-	my_input = int(input('Tell me your age:'))
-	print(f'---You will turn {my_input+1} next year')
+	try:
+		my_input = int(input('Tell me your age:'))
+		print(f'---You will turn {my_input+1} next year')
+	except KeyboardInterrupt:
+		print('\nEnding')
+		break
